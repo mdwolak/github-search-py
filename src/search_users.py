d35d665e09e5fedaefedb9ff4b79640a4a605847
mdwolak <mdwolak@googlemail.com>
2024-02-21 14:04:25 +0000

    refactor: Clean up search_users

--- src/search_users.py
+++ src/search_users.py
@@ -4,20 +4,20 @@ from dotenv import dotenv_values
 
 config = dotenv_values('.env')
 token = config.get('GITHUB_AUTH_TOKEN')
-url = 'https://api.github.com/graphql'
+GRAPHQL_ENDPOINT = 'https://api.github.com/graphql'
 headers = {'Authorization': f'Bearer {token}'}
 
 
 # Function to execute the GraphQL query with pagination
-def fetch_data(query, headers, variables, extract_data):
+def fetch_data_paged(query, headers, variables):
     has_next_page = True
     while has_next_page:
+        json = {'query': query, 'variables': variables}
         response = requests.post(
-            url, json={'query': query, 'variables': variables}, headers=headers, timeout=10)
+            GRAPHQL_ENDPOINT, json=json, headers=headers, timeout=10)
         data = response.json()
-        extracted_data = extract_data(data)
-        if extracted_data:
-            yield from extracted_data
+        if data:
+            yield data
         else:
             print("No data found.")
             return
@@ -27,39 +27,31 @@ def fetch_data(query, headers, variables, extract_data):
         variables['after'] = page_info['endCursor']
 
 
-# Function to extract user data from the response
-def extract_users(data):
-    edges = data['data']['search']['edges']
-    if edges:
-        for edge in edges:
-            node = edge['node']
-            yield node
-    else:
-        return
+def get_query(file_name):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(dir_path, file_name)
 
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
 
-# read the query from the file
-dir_path = os.path.dirname(os.path.realpath(__file__))
-file_path = os.path.join(dir_path, 'search_users.gql')
-
-with open(file_path, 'r', encoding='utf-8') as file:
-    getSearchResultsQuery = file.read()
 
 # run the query to retrieve users
 variables = {"query": "location:Poland language:TypeScript type:user followers:>100",
              "type": "USER",
              "after": None,
              "perPage": 10}
-for user in fetch_data(getSearchResultsQuery, headers, variables, extract_users):
-    print(f"Username: {user['login']}")
-    print(f"Location: {user['location']}\n")
-
-# Run the query to retrieve repositories
-# searchType = "REPOSITORY"
-# variables = {"searchQuery": searchQuery, "searchType": searchType, "after": after_cursor, "perPage": 10}
-# for repo in fetch_data(getSearchResultsQuery, headers, variables, extract_repos):
-#     print(f"Repository Name: {repo['name']}")
-#     print(f"Description: {repo['description']}\n")
-
+for page in fetch_data_paged(get_query("search_users.gql"), headers, variables):
+    edges = page['data']['search']['edges']
+    if edges:
+        for edge in edges:
+            node = edge['node']
+            print(node)
+            # print(f"Username: {node['login']}")
+            # print(f"Location: {node['location']}\n")
 
 print("No more pages.")
+
+# CREATE TABLE cursors (
+#     id SERIAL PRIMARY KEY,
+#     end_cursor VARCHAR(255) NOT NULL
+# );
